a81101829c814323bf5fe7f55aaa94195962420e
dev1110-Ai <bhatt30116@gmail.com>
2024-07-25 14:14:28 +0530

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf(/n"welcome");
+    printf(/n"my name is dev");
+    printf(/n"i am it student");
+    printf(/n"i like coding");
+    printf(/n"error,.01");
+}
+   
+    
