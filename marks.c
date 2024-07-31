bd3081e66acbf79d41c71dc51ce941a4501d28b6
dev <bhatt30116@gmail.com>
2024-07-31 22:16:08 +0530

    Add files via upload

---
+++ marks.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int studentMarks = 99;
+    int failInExam = 09;
+
+    
+    if (studentMarks < failInExam)
+   { 
+        printf("The student has failed in the exam.\n");
+    } else 
+	{
+        printf("The student has passed in the exam.\n");
+    }
+
+    return 0;
+}
